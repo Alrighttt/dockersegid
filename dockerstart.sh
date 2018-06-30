f9358b349c266c705a0d3c1859675b0ed330322b
Alrighttt <36680730+Alrighttt@users.noreply.github.com>
2018-06-30 05:26:06 -0400

    remove 127.0.0.1 $SEEDIP

--- dockerstart.sh
+++ dockerstart.sh
@@ -5,7 +5,6 @@ SEGID=$2
 
 USER=RPCUSER
 PASSWD=RPCPASS98fjf9ufj9uj9dj3di3jdi3dgsfouoii2oii2oi2oi
-SEEDIP=127.0.0.1
 
 docker run -d --rm --name $SEGID -ti \
   --mount "src=$SEGID,dst=/home/komodo/.komodo" \
@@ -20,4 +19,4 @@ docker run -d --rm --name $SEGID -ti \
   -ac_supply=10000000 \
   -ac_staked=90 \
   -ac_reward=1000000000 \
-  -addnode=$SEEDIP
+  -addnode=1.1.1.1
