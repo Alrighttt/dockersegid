b4d35f19a81bed0a361203d092aab069795d3b94
Alrighttt <mzlot555@gmail.com>
2018-06-30 05:46:49 -0400

    Revert "remove 127.0.0.1 $SEEDIP"
    This reverts commit f9358b349c266c705a0d3c1859675b0ed330322b.

--- dockerstart.sh
+++ dockerstart.sh
@@ -5,6 +5,7 @@ SEGID=$2
 
 USER=RPCUSER
 PASSWD=RPCPASS98fjf9ufj9uj9dj3di3jdi3dgsfouoii2oii2oi2oi
+SEEDIP=127.0.0.1
 
 docker run -d --rm --name $SEGID -ti \
   --mount "src=$SEGID,dst=/home/komodo/.komodo" \
@@ -19,4 +20,4 @@ docker run -d --rm --name $SEGID -ti \
   -ac_supply=10000000 \
   -ac_staked=90 \
   -ac_reward=1000000000 \
-  -addnode=1.1.1.1
+  -addnode=$SEEDIP
